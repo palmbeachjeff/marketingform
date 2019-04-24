09b6f7937a51df2f878e14f1ace6418a8775c875
semicolon jeff <palmbeachjeff@gmail.com>
2019-04-24 07:35:44 -0400

    got rid of duplicate PHP on page.

--- process.php
+++ process.php
@@ -35,34 +35,3 @@
   ?> 
   
 </body>
-</html><!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Cool Form</title>
-</head>
-<body>
-
-  <?php
-    $name = $_POST["name"];
-    echo "Hello, " . $name;
-    echo "<br>";
-
-    $company = $_POST["company"];
-    echo "\nDo you own " . $company . "?";
-    echo "<br>";
-
-    $email = $_POST["emailaddress"];
-    echo "\nEmail Address: " . $email; 
-    echo "<br>";
-
-    date_default_timezone_set('UTC');
-
-    echo "We received your information at: ";
-    echo date('h:i:s a, l F jS Y');
-  ?> 
-  
-</body>
-</html>
